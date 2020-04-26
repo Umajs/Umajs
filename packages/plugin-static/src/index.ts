0465b43e45d6ca5bb2313871b47876ce6b4e526c
EdwardZZZ <zzh627@126.com>
2020-04-26 17:41:42 +0800

    style: add types

--- packages/plugin-static/src/index.ts
+++ packages/plugin-static/src/index.ts
@@ -1,6 +1,6 @@
 import * as Koa from 'koa';
 import * as koaStatic from 'koa-static';
 
-export default function (ursa: any, options: any = {}): Koa.Middleware {
+export default function (ursa: any, options: { root: string, opts?: koaStatic.Options }): Koa.Middleware {
     return koaStatic(options.root, options.opts);
 }
