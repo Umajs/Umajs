c52cdf8c61332929c201ba98af68a0a83b4d164d
EdwardZZZ <zzh627@126.com>
2020-05-01 20:41:29 +0800

    perf(static): add default value

--- packages/plugin-static/src/index.ts
+++ packages/plugin-static/src/index.ts
@@ -1,8 +1,8 @@
 import * as Koa from 'koa';
 import * as koaStatic from 'koa-static';
 
-export default function (ursa: any, options: { root: string, opts?: koaStatic.Options } = {
-    root: './static',
-}): Koa.Middleware {
-    return koaStatic(options.root, options.opts);
+export default function (ursa: any, options: { root: string, opts?: koaStatic.Options }): Koa.Middleware {
+    const { root = './static', opts } = options;
+
+    return koaStatic(root, opts);
 }
