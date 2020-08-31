4114f2e26454805d9b661c508786e6eb4d1507d2
EdwardZZZ <zzh627@126.com>
2020-08-31 15:15:15 +0800

    perf(config): freeze config

--- packages/core/src/core/Uma.ts
+++ packages/core/src/core/Uma.ts
@@ -77,6 +77,7 @@ export default class Uma {
 
     loadConfig() {
         ConfigLoader.loadConfigDir(this.options.configPath);
+        Object.freeze(ConfigLoader.config);
         this.config = ConfigLoader.config;
     }
 
