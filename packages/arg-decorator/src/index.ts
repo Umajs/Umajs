0d9a1c76e721c5ddd9bec5478a39aae172900122
dazjean <zunyi_zjj@163.com>
2020-09-22 17:39:48 +0800

    feat: lerna ignore ar-decorator

--- packages/arg-decorator/src/index.ts
+++ packages/arg-decorator/src/index.ts
@@ -1,11 +1,11 @@
-import Uma, { createArgDecorator, IContext, mixin } from '@umajs/core';
+import { createArgDecorator, IContext } from '@umajs/core';
 
 import Tips from './Tips';
 
-const uma = Uma.instance();
+// const uma = Uma.instance();
 
 // 可以通过 argDecorator.config.ts 配置提示
-mixin(true, Tips, uma.config.argDecorator || {});
+// mixin(true, Tips, uma.config.argDecorator || {});
 
 /**
  * 参数必传
